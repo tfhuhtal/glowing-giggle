b78d555ecd682a2877e597d12835632982cea0ae
tfhuhtal <tuomas.huhtala@icloud.com>
2024-05-05 15:40:26 +0300

    more initializing

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-
 const app = express();
+app.use(express.json());
+
+const PORT = 3000;
 
 app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
   res.send('pong');
 });
 
-const PORT = 3003;
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
